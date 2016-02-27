839cd1c622cb1806cf5656f63f24159eb06a1bd4
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-02-27 18:35:35 +0100

    image updating now works

--- kordecms/views.py
+++ kordecms/views.py
@@ -99,13 +99,11 @@ class ArticleMixin(object):
     def perform_create(self, serializer):
         # Try to get the file, return None if not found.
         file = self.request.FILES.get('file')
-        print(file)
         serializer.save(author=self.request.user, thumbnail_image_src=file)
 
     def perform_update(self, serializer):
         # Try to get the file, return None if not found.
         file = self.request.FILES.get('file')
-        print(file)
         serializer.save(thumbnail_image_src=file)
 
 

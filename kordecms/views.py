743f3776417596c6d6c024a3bb5da9695f234956
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-02-27 18:06:45 +0100

    moved perform update

--- kordecms/views.py
+++ kordecms/views.py
@@ -102,19 +102,18 @@ class ArticleMixin(object):
         print(file)
         serializer.save(author=self.request.user, thumbnail_image_src=file)
 
-    def perform_update(self, serializer):
-        # Try to get the file, return None if not found.
-        file = self.request.FILES.get('file')
-        print(file)
-        serializer.save(thumbnail_image_src=file)
-
 
 class ArticleList(ArticleMixin, generics.ListCreateAPIView):
     pass
 
 
 class ArticleDetail(ArticleMixin, generics.RetrieveUpdateDestroyAPIView):
-    pass
+
+    def perform_update(self, serializer):
+        # Try to get the file, return None if not found.
+        file = self.request.FILES.get('file')
+        print(file)
+        serializer.save(thumbnail_image_src=file)
 
 
 class ArticleElementList(generics.ListCreateAPIView):

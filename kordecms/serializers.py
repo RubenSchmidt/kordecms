5abae1c6c74d49f6e5eddcbe55851b382ccb4609
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-02-28 17:21:56 +0100

    removed unused stuff

--- kordecms/serializers.py
+++ kordecms/serializers.py
@@ -81,11 +81,6 @@ class ArticleSerializer(TaggitSerializer, serializers.ModelSerializer):
             'id', 'title', 'author_name', 'body_text', 'thumbnail_image_url', 'created_at', 'tags', 'author',
             'elements', 'slug', 'is_published', 'is_internal')
 
-    def get_validation_exclusions(self):
-
-        exclusions = super(ArticleSerializer, self).get_validation_exclusions()
-        return exclusions + ['tags']
-
 
 class ArticleCommentSerializer(serializers.ModelSerializer):
     class Meta:
